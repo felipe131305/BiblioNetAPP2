5628b69044a923cdc905784d59026071f07a8fa0
felipe131305 <felipe08119617@gmail.com>
2022-03-17 12:32:38 -0500

    firts commit

---
+++ BiblioNetAPP2/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using BiblioNetAPP2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioNetAPP2.Controllers
+{
+    public class BookController: Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(book);
+            }
+            return View();
+        }
+
+    }
+}
---
+++ BiblioNetAPP2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BiblioNetAPP2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
